24e2d51eee735e8b976799150ed4062c5addcbea
dhruv <dhruv@Roger>
2021-08-02 10:53:45 -0400

    this is first commit and push of simple java project

---
+++ Simplejava/src/main/java/TestProject/Simplejava/App.java
@@ -0,0 +1,18 @@
+package TestProject.Simplejava;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Samael Bate (singingbush)
+ * created on 10/10/18
+ */
+public class App {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(final String[] args) {
+        log.info("Hello World!");
+        System.out.println("this is a new start. Dhruv is going to change his world now");
+    }
+}
